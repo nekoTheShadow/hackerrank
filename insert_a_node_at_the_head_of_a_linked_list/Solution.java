96f92eb5c541b0f1c8f739b54ee23f4cb12e728e
Hajime NAKAMURA <h.nakamura0903@gmail.com>
2020-05-31 00:16:03 +0900

    Insert a node at the head of a linked list

---
+++ insert_a_node_at_the_head_of_a_linked_list/Solution.java
@@ -0,0 +1,63 @@
+package insert_a_node_at_the_head_of_a_linked_list;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the insertNodeAtHead function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode llist, int data) {
+        SinglyLinkedListNode head = new SinglyLinkedListNode(data);
+        if (llist != null) {
+            head.next = llist;
+        }
+        return head;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+}
